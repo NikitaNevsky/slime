f828781fd6b63769ba4f29a528e673fb26960422
Nikita Nevskii <40342871+NikitaNevsky@users.noreply.github.com>
2024-05-18 22:00:40 +0300

    Update Project

--- js/script.js
+++ js/script.js
@@ -1,39 +1,10 @@
 $(document).ready(function(){
-    $(".positive__slider .owl-carousel").owlCarousel({
+    $(".reviews__slider").owlCarousel({
         loop: true,
         margin: 10,
         nav: true,
-        dots: true,
+        dots: false,
         items: 1
     });
 
-    $(".doctors__list .owl-carousel").owlCarousel({
-        loop: true,
-        margin: 10,
-        nav: true,
-        dots: true,
-        responsive:{
-            0:{
-                items:1
-            },
-            600:{
-                items:3
-            }
-        }
-    });
-
-    $(".icon-menu").click(function() {
-        $(".menu").addClass('show');
-        $("body").addClass('no-scroll');
-    });
-
-    $(".menu__info--close").click(function() {
-        $(".menu").removeClass('show');
-        $("body").removeClass('no-scroll');
-    });
-
-    $(".menu-overlay").click(function() {
-        $(".menu").removeClass('show');
-        $("body").removeClass('no-scroll');
-    });
 });
