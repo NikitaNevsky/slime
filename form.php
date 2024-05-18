c6674a39f68b2f7fb658f98203280f6ecb512e15
Nikita Nevskii <40342871+NikitaNevsky@users.noreply.github.com>
2024-05-19 01:20:02 +0300

    Update Form Files

---
+++ form.php
@@ -0,0 +1,12 @@
+<?php
+    // Получаем данные из полей ввода
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $phone = $_POST['phone'];
+    $to = 'ox.cmex@mail.ru';
+    $header = 'Заявка на франшизу "Фабрика слаймов"'
+    // Собираем все данные в письмо
+    $message = "Имя пользователя: $name \nЭлектронная почта: $email \nТелефон: $phone";
+    // Отправляем письмо
+    $send = mail($to, $header, $phone, "Content-type:text/plain; charset = UTF-8\r\nFrom:$email");
+?>
