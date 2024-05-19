f3d365f47a3d0cf44a69dec42a05d7e5b878be72
Nikita Nevskii <40342871+NikitaNevsky@users.noreply.github.com>
2024-05-19 18:42:49 +0300

    Update CSS/JS

--- js/script.js
+++ js/script.js
@@ -6,5 +6,25 @@ $(document).ready(function(){
         dots: false,
         items: 1
     });
-
 });
+
+$(document).ready(function() {
+ 
+    //E-mail Ajax Send
+    $("form").submit(function() { //Change
+      var th = $(this);
+      $.ajax({
+        type: "POST",
+        url: "/mail.php", //Change
+        data: th.serialize()
+      }).done(function() {
+        swal("Отлично!", "Ваша заявка успешно принята!", "success");
+        setTimeout(function() {
+          // Done Functions
+          th.trigger("reset");
+        }, 1000);
+      });
+      return false;
+    });
+   
+  });
